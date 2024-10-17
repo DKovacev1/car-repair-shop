b0fcd4dd3369c5f9443ea19d5ad7f8136bf8dd6b
dkovacev <dkovacev@fina.hr>
2024-10-17 13:42:29 +0200

    extended exception handling

--- src/main/java/hr/autorepair/shop/exception/handler/CustomExceptionHandler.java
+++ src/main/java/hr/autorepair/shop/exception/handler/CustomExceptionHandler.java
@@ -5,17 +5,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.SQLException;
+import java.sql.SQLSyntaxErrorException;
+
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    @ExceptionHandler(value = {SQLException.class, Exception.class, RuntimeException.class, SQLSyntaxErrorException.class, BadSqlGrammarException.class})
+    public ResponseEntity<ErrorResponse> handleExceptions(Exception ex, HttpServletRequest request){
+        printStackTrace(ex);
+
+        ErrorResponse errorResponse = new ErrorResponse.Builder()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message("Unexpected error has occurred. Please, contact the administrator.")
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.internalServerError().body(errorResponse);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        printStackTrace(ex);
         FieldError firstError = ex.getBindingResult().getFieldErrors().stream().findFirst().orElse(null);
         String message;
         if (firstError != null)
@@ -34,7 +52,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(HttpMessageNotReadableException ex, HttpServletRequest request) {
-
+        printStackTrace(ex);
         ErrorResponse errorResponse = new ErrorResponse.Builder()
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .message(ex.getMessage())
@@ -46,6 +64,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex, HttpServletRequest request){
+        printStackTrace(ex);
         ErrorResponse errorResponse = new ErrorResponse.Builder()
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .message(ex.getLocalizedMessage())
@@ -53,6 +72,10 @@ public class CustomExceptionHandler {
                 .build();
 
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getStatusCode().value()));
+    }
+
+    private void printStackTrace(Exception ex){
+        ex.printStackTrace();
     }
 
 }
