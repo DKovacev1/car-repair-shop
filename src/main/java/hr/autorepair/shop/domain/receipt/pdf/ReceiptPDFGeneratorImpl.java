e1e187cefbab756db61545c6f5ed696b7dc672c5
dkovacev <dkovacev@fina.hr>
2024-10-21 15:16:15 +0200

    pdf - test 2

--- src/main/java/hr/autorepair/shop/domain/receipt/pdf/ReceiptPDFGeneratorImpl.java
+++ src/main/java/hr/autorepair/shop/domain/receipt/pdf/ReceiptPDFGeneratorImpl.java
@@ -6,10 +6,15 @@ import hr.autorepair.shop.exception.exceptions.BadRequestException;
 import hr.autorepair.shop.secutiry.model.UserPrincipal;
 import hr.autorepair.shop.util.UserDataUtils;
 import lombok.AllArgsConstructor;
-import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.util.JRLoader;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import static hr.autorepair.common.constants.MessageConstants.RECEIPT_NOT_EXIST;
 
@@ -19,7 +24,8 @@ public class ReceiptPDFGeneratorImpl implements ReceiptPDFGenerator {
 
     private final ReceiptRepository receiptRepository;
 
-    private static final String COMPILED_JASPER_URL = "/report/compiled/blank.jasper";
+    private static final String RECEIPT_COMPILED_JASPER_URL = "/report/compiled/receipt.jasper";
+    private static final String LOGO = "/report/images/logo.ico";
 
     @Override
     public byte[] generatePdf(Long idReceipt) throws JRException {
@@ -36,11 +42,13 @@ public class ReceiptPDFGeneratorImpl implements ReceiptPDFGenerator {
                     });
         }
 
-        /*File file = new File(Objects.requireNonNull(getClass().getResource(COMPILED_JASPER_URL)).getFile());
+        Map<String, Object> params = new HashMap<>();
+        params.put("tekst", "PROBA");
+
+        File file = new File(Objects.requireNonNull(getClass().getResource(RECEIPT_COMPILED_JASPER_URL)).getFile());
         JasperReport jasperDesign = (JasperReport) JRLoader.loadObject(file);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperDesign, new HashMap<>(), new JREmptyDataSource());
-        return JasperExportManager.exportReportToPdf(jasperPrint);*/
-        return new byte[0];
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperDesign, params, new JREmptyDataSource());
+        return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 
 }
